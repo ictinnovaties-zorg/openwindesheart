03f61ec9ccec8cb6f5e384c1211e908054f6fc8e
marstc <51233523+marstc@users.noreply.github.com>
2019-10-11 11:17:26 +0200

    empty branche master
    lol

--- WindesHeart/WindesHeart/Model/CarouselItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WindesHeart.Model
-{
-    public class CarouselItem
-    {
-        public string Title { get; set; }
-        public string Text { get; set; }
-        public string Image { get; set; }
-    }
-}
