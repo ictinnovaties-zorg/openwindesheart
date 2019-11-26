3d65487dcf182ea458a55d4f61ebaf54b555b133
Hielke <s1104408@student.windesheim.nl>
2019-11-26 12:24:47 +0100

    improvements

--- WindesHeartApp/WindesHeartApp/Views/StepsPage.xaml.cs
+++ WindesHeartApp/WindesHeartApp/Views/StepsPage.xaml.cs
@@ -2,6 +2,7 @@
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using WindesHeartApp.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -63,14 +64,38 @@ namespace WindesHeartApp.Pages
         {
             DateTime today = DateTime.Now;
             DayOfWeek day = today.DayOfWeek;
+            DateTimeFormatInfo dtfi = new CultureInfo("en-US").DateTimeFormat;
             float y = 0.85f;
-            Day1Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-6).DayOfWeek.ToString(), "Day1Binding", 0.05, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            Day2Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-5).DayOfWeek.ToString(), "Day2Binding", 0.20, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            Day3Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-4).DayOfWeek.ToString(), "Day3Binding", 0.35, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            Day4Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-3).DayOfWeek.ToString(), "Day4Binding", 0.50, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            Day5Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-2).DayOfWeek.ToString(), "Day5Binding", 0.65, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            Day6Button = PageBuilder.AddButton(absoluteLayout, today.AddDays(-1).DayOfWeek.ToString(), "Day6Binding", 0.80, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
-            TodayButton = PageBuilder.AddButton(absoluteLayout, "TODAY", "TodayBinding", 0.95, y, 0.15, 0.1, AbsoluteLayoutFlags.All);
+            int fontSize = 12;
+            int cornerRadius = 100;
+
+            Day1Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-6).DayOfWeek), "Day1Binding", 0.05, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day1Button.CornerRadius = cornerRadius;
+            Day1Button.FontSize = fontSize;
+
+            Day2Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-5).DayOfWeek), "Day2Binding", 0.20, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day2Button.CornerRadius = cornerRadius;
+            Day2Button.FontSize = fontSize;
+
+            Day3Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-4).DayOfWeek), "Day3Binding", 0.35, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day3Button.CornerRadius = cornerRadius;
+            Day3Button.FontSize = fontSize;
+
+            Day4Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-3).DayOfWeek), "Day4Binding", 0.50, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day4Button.CornerRadius = cornerRadius;
+            Day4Button.FontSize = fontSize;
+
+            Day5Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-2).DayOfWeek), "Day5Binding", 0.65, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day5Button.CornerRadius = cornerRadius;
+            Day5Button.FontSize = fontSize;
+
+            Day6Button = PageBuilder.AddButton(absoluteLayout, dtfi.GetAbbreviatedDayName(today.AddDays(-1).DayOfWeek), "Day6Binding", 0.80, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            Day6Button.CornerRadius = cornerRadius;
+            Day6Button.FontSize = fontSize;
+
+            TodayButton = PageBuilder.AddButton(absoluteLayout, "Today", "TodayBinding", 0.95, y, 0.1, 0.05, AbsoluteLayoutFlags.All);
+            TodayButton.CornerRadius = cornerRadius;
+            TodayButton.FontSize = fontSize;
             TodayButton.IsEnabled = false;
         }
 
@@ -84,11 +109,25 @@ namespace WindesHeartApp.Pages
             PageBuilder.AddLabel(absoluteLayout, "Steps", 0.10, 0.10, Globals.lighttextColor);
             PageBuilder.AddReturnButton(absoluteLayout, this);
 
-            var previousBtn = PageBuilder.AddButton(absoluteLayout, "Previous", "PreviousDayBinding", 0.1, 0.15, 0.25, 0.07, AbsoluteLayoutFlags.All);
-            previousBtn.FontSize = 12;
+            ImageButton previousBtn = new ImageButton
+            {
+                Source = "arrow_left.png",
+                BackgroundColor = Color.Transparent
+            };
+            AbsoluteLayout.SetLayoutFlags(previousBtn, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(previousBtn, new Rectangle(0.3, 0.135, 0.1, 0.1));
+            previousBtn.SetBinding(Button.CommandProperty, new Binding() { Path = "PreviousDayBinding" });
+            absoluteLayout.Children.Add(previousBtn);
 
-            var nextBtn = PageBuilder.AddButton(absoluteLayout, "Next", "NextDayBinding", 0.9, 0.15, 0.25, 0.07, AbsoluteLayoutFlags.All);
-            nextBtn.FontSize = 12;
+            ImageButton nextBtn = new ImageButton
+            {
+                Source = "arrow_right.png",
+                BackgroundColor = Color.Transparent
+            };
+            AbsoluteLayout.SetLayoutFlags(nextBtn, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(nextBtn, new Rectangle(0.7, 0.135, 0.1, 0.1));
+            nextBtn.SetBinding(Button.CommandProperty, new Binding() { Path = "NextDayBinding" });
+            absoluteLayout.Children.Add(nextBtn);
 
             CurrentDayLabel = PageBuilder.AddLabel(absoluteLayout, "Today", 0.5, 0.16, Color.Black);
             CurrentDayLabel.FontSize = 15;
@@ -104,7 +143,7 @@ namespace WindesHeartApp.Pages
             //ToggleRealTimeStepsButton = PageBuilder.AddButton(absoluteLayout, "Enable realtime steps", "ToggleRealTimeStepsBinding", 0.2, 0.38, 300, 50);
 
             AbsoluteLayout.SetLayoutFlags(chartView, AbsoluteLayoutFlags.All);
-            AbsoluteLayout.SetLayoutBounds(chartView, new Rectangle(0.5, 0.20, 0.65, 0.65));
+            AbsoluteLayout.SetLayoutBounds(chartView, new Rectangle(0.5, 0.25, 0.60, 0.60));
             absoluteLayout.Children.Add(chartView);
 
             var label2 = PageBuilder.AddLabel(absoluteLayout, "0 Kcal", 0.5, 0.65, Color.Black);
