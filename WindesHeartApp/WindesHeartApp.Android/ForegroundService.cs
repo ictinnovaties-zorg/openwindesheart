5d955b6e339a6644060574463cc4e9b4f7738120
Kevin van Sloten <kevinvansloten@home.nl>
2019-12-18 14:01:57 +0100

    init

---
+++ WindesHeartApp/WindesHeartApp.Android/ForegroundService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WindesHeartApp.Droid
+{
+	class ForegroundService : Service
+	{
+		public override IBinder OnBind(Intent intent)
+		{
+			// Return null because this is a pure started service. A hybrid service would return a binder that would
+			// allow access to the GetFormattedStamp() method.
+			return null;
+		}
+	}
+}
