8cd43a51915ff2a10e631994b8b68e666c44e077
Gido Hakvoort <gh0073874@windesheim.nl>
2020-01-27 14:24:53 +0100

    Update Copyright details

--- OpenWindesheart/Devices/MiBand3/Services/MiBand3ConfigurationService.cs
+++ OpenWindesheart/Devices/MiBand3/Services/MiBand3ConfigurationService.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright 2020 Research group ICT innovations in Health Care
+﻿/* Copyright 2020 Research group ICT innovations in Health Care, Windesheim University of Applied Sciences
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
